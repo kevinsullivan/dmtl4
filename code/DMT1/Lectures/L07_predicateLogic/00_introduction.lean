150c6f97ab4a843fb10e8014ef9b6203b129603a
Kevin Sullivan <sullivan@virginia.edu>
2025-02-14 23:07:35 -0500

    Clean up predicate logic chapter

--- code/DMT1/Lectures/L07_predicateLogic/00_introduction.lean
+++ code/DMT1/Lectures/L07_predicateLogic/00_introduction.lean
@@ -16,7 +16,8 @@ theory: sets of objects and ways of speaking about *all* of
 the elements in a given set, or *at least one* element of a
 given set as satisfying some condition.
 
-## Syntax
+
+## Overview
 
 Predicate logic  provides syntactic symbols to refer to such
 things, including *constant* and *variable* symbols (to refer
@@ -28,10 +29,11 @@ Prredicate logic adopts the logical operators and their usual
 truth-functional meanings from propositional logic, but now as
 composing smaller expressions in predicate logic.
 
+## Examples
 
-## Example
+We'll consider two examples.
 
-For example, one might consider simple world having;
+### A String Length Property
 
 - character strings and natural numbers as *entities*
 - a *set* of strings, e.g., containing three particular strings
@@ -62,7 +64,7 @@ worls for, yielding propositions that are then subject to
 ordinary proof construction efforts.
 
 
-## Example
+### No Largest Natural Number
 
 As an example in first-order predicate logic, we can
 write the expression, *∀n ∃m (m > n + 1)*. In English
@@ -79,6 +81,8 @@ expression as being true or not in that domain.
 To illustrate, we'll informally describe two different
 domains and interpretations for our example expression.
 
+#### One Interpretation
+
 First, we will take the domain to be the universe of
 natural number arithmetic. Natural numbers, functions,
 and relations will be the entities in the domain. We
@@ -93,6 +97,8 @@ number, *n*, there is some natural number *m*, such that
 *m* is greater than one more than *n*. It's easy to see
 that the expression can be judged true when evaluated
 over this domain structure.
+
+#### A Different Interpretation
 
 On the other hand, the entities of the domain could be
 people; *m > n* could be the relation that holds exactly
@@ -121,25 +127,7 @@ Lean would know to take the function, *Nat.add*, as the
 meaning of *+*, and the *Nat.lt* relation as the meaning
 of *<*.
 
-## From Propositional to Predicate
-
-With that introduction, we now highlight key differences
-between propositional and predicate logic, focusing on
-differences that hold whether one is speaking of simple
-first-order predicate logic or the much mroe expressive
-higher-order predicate logic provide by the Lean prover.
-
-### Syntax of Predicate Logic
-
-The syntax of predicate logic keeps the fixed operator symbols
-and variable expressions of propositional logic. To these it adds
-quantifier, constant, function, and predicate symbols. Here are
-examples, with each example written first in first-order logic and
-then in Lean.
-
-TODO!
-
-### Semantics of Predicate Logic
+### Something About People
 
 As an example, domain could have *people* as entities. It could have
 a function, *motherOf*, that maps any give person, *p*, to the person
@@ -159,12 +147,14 @@ would be more room for uncaught implicit type errors.
 
 - ∀ p, q, isPerson p → isPerson q → isBlue p → isMother q p → isBlue q
 
-### Propositional Logic Connectives
 
-Predicate logic maintains the logical connectives, such
-as *and*, *or*, and *not* from propositional logic, with
-their usual truth-functional meanings. Beyond that, there
-are significant changes.
+## Differences From Propositional to Predicate Logic
+
+With that introduction, we now highlight key differences
+between propositional and predicate logic, focusing on
+differences that hold whether one is speaking of simple
+first-order predicate logic or the much mroe expressive
+higher-order predicate logic provide by the Lean prover.
 
 ### Universal and Existential Quantifers
 
@@ -228,19 +218,19 @@ Object as their "static types". In particular, here
 we define (Object.person n) to be the n'th person in
 whatever world we might be modeling, and (rock n) to
 be the n'th rock.
+@@@ -/
 
-```lean
 inductive Object
 | person (n : Nat)
 | rock (r : Nat)
-```
 
+/- @@@
 In first-order logic, the only way to know what kind
 of object on has been given is to apply a predicate.
 So here are two predicates, the first true for objects
 deemed to be people and the second, rocks.
+@@@ -/
 
-```lean
 def isPerson : Object → Bool
 | Object.person _ => true
 | _ => false
@@ -248,8 +238,8 @@ def isPerson : Object → Bool
 def isRock : Object → Bool
 | Object.rock _ => true
 | _ => false
-```
 
+/- @@@
 Now we'd like to assert that everyone is mortal.
 So we'll define a predicate, which is to say, a
 proposition with one or more arguments that, when
@@ -270,30 +260,31 @@ number and nothing will go wrong--until you *run*
 the program, at which point the argument can be
 runtime-tested to determine what kind of thing it
 is really meant to represent.
+@@@ -/
 
-```lean
 def isMortal : Object → Bool
 | o => if isPerson o then true else false
-```
 
+/- @@@
 We're now ready to use our first order logic.
 First we define some objects: a person named
 *socrates* and a rock that we'll simple call,
 *someRock*.
+@@@ -/
 
-```lean
 def socrates : Object := Object.person 0
 def someRock : Object := Object.rock 0
-```
 
+
+/- @@@
 Now we can ask whether either of these objects
 satisfies the isMortal predicate.
+@@@ -/
 
-```lean
 #eval isMortal socrates   -- yes/true
 #eval isMortal someRock   -- no/false
-```
 
+/- @@@
 But, again, it doesn't even make sense to
 ask if a rock is mortal. If it is then one
 might conclude that at some point in time
@@ -311,15 +302,15 @@ Lean 4.
 First, Person and Rock are now separate types,
 not just differently shaped terms of the same
 type, Object.
+@@@ -/
 
-```lean
 structure Person where
 (n : Nat)
 
 structure Rock where
 (n : Nat)
-```
 
+/- @@@
 Second, we define isMortal' (with a tick mark so
 as not to have a name conflict) as a property of
 values of type Person only. In more detail, here
@@ -329,17 +320,18 @@ of propositions obtained by apply8ing the predicate
 to a Person. The constructor takes any Person, p, as
 an argument and yields a proof of the proposition,
 isMortal' p.
+@@@ -/
 
-```lean
 inductive isMortal' : Person → Prop
 | everyoneMortal: (p : Person) →  isMortal' p
 open isMortal'
-```
 
+
+/- @@@
 Next we define a person entity and a rock entity,
 but now they are of completely unrelated types.
+@@@ -/
 
-```lean
 def quine : Person := Person.mk 0
 def doorStop : Rock := Rock.mk 0
 
@@ -352,6 +344,8 @@ def p0IsMortal : isMortal' quine := everyoneMortal quine
 -- But the question whether a rock is mortal is a type error
 def r0IsMortal : isMortal' doorStop := sorry
 
+/- @@@
+```lean
 application type mismatch
   isMortal' doorStop
 argument
@@ -374,16 +368,20 @@ object. If it's a person, the predicate is true of that
 object; and if it's a rock, it's false for that object.
 In this context, we can express the proposition that all
 people are mortal, as follows:
+@@@ -/
 
-- ∀ x, isPerson x → isMortal x
+#check ∀ x, isPerson x → isMortal x
 
+/- @@@
 But this predicate is true for rocks, isn't it! You
 can see how things can easily go awry when writing
 complex specifications in first-order logic. In the
 typed logic of Lean we can say it easier and better:
+@@@ -/
 
-- ∀ (p : Person), isMortal' p
+#check ∀ (p : Person), isMortal' p
 
+/- @@@
 No runtime ("reasoning-time") test is needed to tell
 if the argument really represents a person or not. The
 type checker of Lean 4 does that for us right up front.
@@ -401,9 +399,11 @@ To express the idea that *all people are mortal*, in first-order
 thus untyped logic, for example, you'd say, For any object, *p*,
 if *p* "is a person" (if *p* satisfies the *isPerson* predicate),
 then *p* satisfies the isMortal predicate.
+@@@ -/
 
-- ∀ p, isPerson p → isMortal p
+#check ∀ p, isPerson p → isMortal p
 
+/- @@@
 The "∀ p" in effect loops over every object in the universe,
 tests each to see if it's a person, and in that case it asserts
 that that particular object, *p*, also satisfies the *isMortal*
@@ -412,9 +412,11 @@ predicate.
 In Lean, by contrast, you would have an inductive type defined,
 called Person, and you would say, given any object, p, "of type"
 Person, p is mortal. Here it is in Lean.
+@@@ -/
 
-- ∀ (p : Person), isMortal p
+#check ∀ (p : Person), isMortal' p
 
+/- @@@
 This proposition asserts that if *p* is *any Person*, then *p*
 is mortal. There is no possibility that *p* could refer to any
 object other than a Person object here, nor than isMoral could
@@ -440,8 +442,6 @@ It will be instead by *deduction* from certain propositions accepted as
 In fact, propositional logic is incorporated into deductive reasoning
 in predicate logic adoption of the propositions in the earlier *axioms*
 as the axioms of predicate logic.
-
-## Example
 
 Suppose for example that we have two propositions in predicate logic,
 P and Q, and we want to show *deductively* that P /\ Q -> Q /\ P. To
